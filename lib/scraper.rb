e766aa6a5ec1c542ddeb81fdc467107e0f3ae20a
Riley Moore <rilemoore@gmail.com>
2019-10-25 19:39:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,7 @@ class Scraper
       new_course = Course.new
       new_course.title = course.css("h2").text
       new_course.schedule = course.css("em").text
-      new_course.schedule = course.css("p").text
+      new_course.description = course.css("p").text
     end
     binding.pry
   end
